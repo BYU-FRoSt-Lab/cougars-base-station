06ea63083d0ec6489deabb1b5556352676718d38
bradenMeyers <braden@themeyers.net>
2025-01-03 16:40:53 -0700

    GMAPs key moved to new file

---
+++ dataplot/plotter_utility/GMAPS_KEY.py
@@ -0,0 +1,6 @@
+# This API key is required in order to use Google's Map Static API.  It is linked to TristanAHodgins@gmail.com,
+# and is free so long as it is used queried less than 240 times per minute.  I don't expect the usage of this script
+# to ever exceed that number, and I see no reason I would deactivate it.  Nonetheless, if the script returns "The API key has
+# not been accepted.  Please verify that it is correct.", you may need to get a new API key from
+# https://www.console.cloud.google.com
+GMAPS_API_KEY = ""
--- dataplot/plotter_utility/config.py
+++ dataplot/plotter_utility/config.py
@@ -4,13 +4,6 @@
 
 # These variables can be changed if you want the program output different data
 
-# This API key is required in order to use Google's Map Static API.  It is linked to TristanAHodgins@gmail.com,
-# and is free so long as it is used queried less than 240 times per minute.  I don't expect the usage of this script
-# to ever exceed that number, and I see no reason I would deactivate it.  Nonetheless, if the script returns "The API key has
-# not been accepted.  Please verify that it is correct.", you may need to get a new API key from
-# https://www.console.cloud.google.com
-GMAPS_API_KEY = ""
-
 # Google maps allows a zoom level between 0 and 21, 0 seeing the global and 21 inspecting individual blades of grass  (that's an 
 # overexageration, to be clear). This is the default value that the script will use for map generation.  It will check to see if 
 # all the points are within the the generated map and adjust until all points are within range.
