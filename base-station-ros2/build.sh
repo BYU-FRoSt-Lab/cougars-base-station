d726d8f9391141d837689f26d151f871942c7a8f
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2025-04-29 12:28:20 -0600

    getting started overlay cougars-ros2 workspace instead of copying packages

---
+++ base-station-ros2/build.sh
@@ -0,0 +1,5 @@
+
+# base-station-ros2 uses packages from cougars-ros2
+# So to build the package, we need to build and source from that repository
+# This script is designed to run in the docker container
+
