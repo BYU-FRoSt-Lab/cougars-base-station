000fd89665a153ae9f2c2bc544f84db729303e05
Tristan Hodgins <tristanahodgins@gmail.com>
2025-05-08 14:07:16 -0600

    Made the dependancies more explicit in the setup.py file

--- base-station-ros2/src/base_station_gui/setup.py
+++ base-station-ros2/src/base_station_gui/setup.py
@@ -4,20 +4,24 @@ package_name = 'base_station_gui'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tristanhodgins',
     maintainer_email='tah88@byu.edu',
     description='TODO: Package description',
     license='MIT',
     tests_require=['pytest'],
+    install_requires=[
+        "setuptools",
+        "PyQt6",
+        "graphviz",
+    ],
     entry_points={
         'console_scripts': [
             'main = base_station_gui.main:main'
