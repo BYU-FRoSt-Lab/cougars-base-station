75950afe8e35a25355e7b170425a680e179c9658
Ben Washburn <benrobertwashburn@gmail.com>
2025-05-15 17:09:03 -0600

    got rid of init stuff

--- base-station-ros2/src/base_station_coms/include/base_station_coms/coms_protocol.hpp
+++ base-station-ros2/src/base_station_coms/include/base_station_coms/coms_protocol.hpp
@@ -25,11 +25,6 @@ enum COUG_MSG_ID : uint8_t {
 
     EMERGENCY_SURFACE = 0x50,
     CONFIRM_EMERGENCY_SURFACE = 0x51,
-
-    INIT_COUG = 0x60,
-    CONFIRM_INIT_COUG = 0x61
-
-
 };
 
 
@@ -44,15 +39,6 @@ struct ConfirmEmergencyKill {
 
 struct EmergencySurface {
     COUG_MSG_ID msg = EMERGENCY_SURFACE;
-}__attribute__((packed));
-
-struct InitCoug{
-    static const COUG_MSG_ID msg_id = INIT_COUG;
-}__attribute__((packed));
-
-struct ConfirmInitCoug{
-    static const COUG_MSG_ID msg_id = CONFIRM_INIT_COUG;
-    bool success;
 }__attribute__((packed));
 
 struct ConfirmEmergencySurface {
