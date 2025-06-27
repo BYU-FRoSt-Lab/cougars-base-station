16854b7ad83cd8e3e5c6ea7138dba7bd5ea29125
Seth Ricks <sethricks340@gmail.com>
2025-06-27 17:34:14 -0600

    added logic to ping the vehicles directly, to update wifi widget and control the base station modem

--- base-station-ros2/gui_docker_setup.sh
+++ base-station-ros2/gui_docker_setup.sh
@@ -3,4 +3,5 @@ sudo apt install tmux libxcb-cursor0
 pip install PyQt6 graphviz
 pip install transforms3d
 sudo apt-get install fonts-noto-color-emoji
+sudo apt install iputils-ping
 pip install tkintermapview
