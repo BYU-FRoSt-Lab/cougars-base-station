83e484ac32221e8cd79640fe330b9ab5f36baec9
FRoSt Lab <frostlab.emails@gmail.com>
2025-07-15 19:37:39 -0600

    coug 2 params file... Need to update this

--- base-station-ros2/launch/terminal_launch.py
+++ base-station-ros2/launch/terminal_launch.py
@@ -11,15 +11,16 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
 
     # Get the first path in COLCON_PREFIX_PATH
-    colcon_prefix_path = os.environ.get('COLCON_PREFIX_PATH', '').split(os.pathsep)
-    if not colcon_prefix_path:
-        raise RuntimeError("COLCON_PREFIX_PATH is not set or empty. Make sure your workspace is sourced.")
-    # The first entry is typically the current workspace's 'install' directory
-    workspace_install_dir = colcon_prefix_path[0]
-    # Traverse up to get the workspace root (assuming standard layout)
-    workspace_root = os.path.abspath(os.path.join(workspace_install_dir, '..'))
+    # colcon_prefix_path = os.environ.get('COLCON_PREFIX_PATH', '').split(os.pathsep)
+    # if not colcon_prefix_path:
+    #     raise RuntimeError("COLCON_PREFIX_PATH is not set or empty. Make sure your workspace is sourced.")
+    # # The first entry is typically the current workspace's 'install' directory
+    # workspace_install_dir = colcon_prefix_path[0]
+    # # Traverse up to get the workspace root (assuming standard layout)
+    # workspace_root = os.path.abspath(os.path.join(workspace_install_dir, '..'))
 
-    param_file = os.path.join(workspace_root, 'base_station_params.yaml')
+
+    param_file = os.path.join('/home/frostlab/base_station/base-station-ros2', 'base_station_params.yaml')
 
     return launch.LaunchDescription([
         launch_ros.actions.Node(
