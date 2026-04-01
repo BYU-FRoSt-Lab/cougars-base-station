90c6a0572bd300732e4d87ad7da5473bfe5b5e07
Braden <bjz-github@spwiz.com>
2026-04-01 13:48:22 -0600

    Update params

--- base_station_ros2/base_station_coms/scripts/dummy_publisher.py
+++ base_station_ros2/base_station_coms/scripts/dummy_publisher.py
@@ -7,7 +7,7 @@ from geometry_msgs.msg import Vector3Stamped
 class DummyPublisher(Node):
     def __init__(self):
         super().__init__('dummy_publisher')
-        self.publisher_ = self.create_publisher(Vector3Stamped, '/vector/a', 10)
+        self.publisher_ = self.create_publisher(Vector3Stamped, '/vector', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
 
     def timer_callback(self):
